e2f9abee4006942f86cd5ed5596320757fc300bc
Elton <57287843+eltontay@users.noreply.github.com>
2022-04-19 19:20:02 +0800

    adding bids and bid order

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 - Allows anyone to fetch an ordered side of the book. For bids these should be ordered descendingly (top bid is the highest) and for asks this should be ascendingly (top ask is the lowest)
 - Allows users to post orders to either side of the book. the contract should lock required collateral
 - Allows users to cancel their orders
+
+## Truffle Test Cases in /test
+
+```bash
+truffle compile
+truffle migrate
+truffle test
+```
--- test/Test_OrderBook.js
+++ test/Test_OrderBook.js
@@ -15,14 +15,48 @@ contract('TestOrderBook', function (accounts) {
     orderBook = await OrderBook.deployed();
   });
 
-  it('Place Bid', async () => {
+  it('Place First Bid - 1 ETH', async () => {
     let value = 1;
-    let amount = value * 0.95 * 1000000000000000000;
+    let valueWei = value * 1000000000000000000;
+    let amount = valueWei * 0.95;
 
     let bidPlaced = await orderBook.placeBid(1, {
       from: person1,
-      value: 1000000000000000000,
+      value: valueWei,
     });
     assert.equal(await orderBook.getBidValue(1, { from: person1 }), amount);
+  });
+
+  it('Place Second Bid - 5 ETH', async () => {
+    let value = 5;
+    let valueWei = value * 1000000000000000000;
+    let amount = valueWei * 0.95;
+
+    let bidPlaced = await orderBook.placeBid(5, {
+      from: person1,
+      value: valueWei,
+    });
+    assert.equal(await orderBook.getBidValue(2, { from: person1 }), amount);
+  });
+
+  it('Place Third Bid - 4 ETH', async () => {
+    let value = 4;
+    let valueWei = value * 1000000000000000000;
+    let amount = valueWei * 0.95;
+
+    let bidPlaced = await orderBook.placeBid(4, {
+      from: person1,
+      value: valueWei,
+    });
+    assert.equal(await orderBook.getBidValue(3, { from: person1 }), amount);
+  });
+
+  it('Check Bid Order', async () => {
+    let bidOrder = await orderBook.fetchBid({
+      from: person1,
+    });
+
+    let correctOrder = ' 2 3 1';
+    assert.equal(bidOrder, correctOrder);
   });
 });
